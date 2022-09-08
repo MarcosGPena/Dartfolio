a0499306a7a902d15255c5c9ea99923395407def
marcos <marrcospena@gmail.com>
2022-09-08 20:42:56 -0300

    cadastro de empresa

--- projeto_dart/bin/projeto_dart.dart
+++ projeto_dart/bin/projeto_dart.dart
@@ -2,4 +2,8 @@ import 'package:projeto_dart/projeto_dart.dart' as projeto_dart;
 
 void main() {
   print('O resultado é : ${projeto_dart.calculate()}!');
+
+  print("---------");
+  print("Cadastre sua empresa");
+  print("---------");
 }
