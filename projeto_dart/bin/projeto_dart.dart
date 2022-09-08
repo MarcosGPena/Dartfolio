d40674e9940abb4e4b097a12213db533bb142647
marcos <marrcospena@gmail.com>
2022-09-08 16:25:00 -0300

    alteracao do arquivo bin/projeto_dart.dart

--- projeto_dart/bin/projeto_dart.dart
+++ projeto_dart/bin/projeto_dart.dart
@@ -1,5 +1,5 @@
 import 'package:projeto_dart/projeto_dart.dart' as projeto_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${projeto_dart.calculate()}!');
+  print('O resultado é : ${projeto_dart.calculate()}!');
 }
